e56079a82c2c254c5551700b83e9d21ca0785f76
Tobias Thiemann <tobithieman@gmail.com>
2024-03-11 22:07:17 +0100

    Create Build.sh

---
+++ Build.sh
@@ -0,0 +1,12 @@
+if docker login 2>/dev/null | grep -q "Login Succeeded"
+then
+    echo "already logged into Docker HUb"
+else
+    docker login
+fi
+
+read -p "TAG_NAME:" tag_name
+
+docker build -t "tabisch/discordmusicbot:$tag_name" -t "tabisch/discordmusicbot:latest" .
+
+docker push "tabisch/discordmusicbot:$tag_name" "tabisch/discordmusicbot:latest"
